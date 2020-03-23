5e8d7940b2ff192f1fddc148d0bc216e318514d5
Nikolay Ovcharenko <unicorn_nels@mail.ru>
2020-03-23 11:55:45 +0300

    Добавление кнопок и ответ
    Добавление кнопок и ответ

--- app/src/main/java/com/example/quizactivity/MainActivity.java
+++ app/src/main/java/com/example/quizactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.quizactivity;
 
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Gravity.TOP =
+                //Toast.setGravity(Gravity.TOP,100,100);
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
